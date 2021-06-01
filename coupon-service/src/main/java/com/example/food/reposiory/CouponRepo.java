45c7018b05cd0fb9eda635ec628b72d049954e6d
abhishekjain0112 <abhishekjain0112@gmail.com>
2021-06-01 10:53:35 +0530

    banking microservices

--- coupon-service/src/main/java/com/example/food/reposiory/CouponRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.food.reposiory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.food.model.Coupon;
-
-public interface CouponRepo extends JpaRepository<Coupon, Long>{
-	Coupon findByCouponCode(String couponCode);
-}
